f96ac32feab5fb12987e4baa30bfb4903b9e137d
jithjayan <jithjayan9341@gmail.com>
2024-12-04 09:15:25 +0530

    javascript start

---
+++ samp.js
@@ -0,0 +1,73 @@
+// alert("welcome")
+// a=10
+// alert(a)
+
+// ---var let const--
+
+// var a=10
+// var a=20
+
+// let a=10
+//  a=20
+
+// const a=10
+// a=20
+
+// console.log(a);
+
+// -------data type-------
+
+// let a=10
+// let a=10.22
+// let a="abcd"
+// let a="210"
+// let a=[1,23,4,5]
+// let a={name:'eve',age:'18'}
+// let a=null
+// let a=true
+// let a=false
+
+// console.log(typeof a);
+// console.log(a);
+
+// ------------operators----------
+
+// +,-,/,*,%,**,++,--  
+
+// console.log(5+2);
+// console.log(5**2);
+// let a=10
+// a++
+// console.log(a);
+
+// +=,-=,*=,/=,**=
+
+// let a=10
+// console.log(a+=2);
+// console.log(a-=2);
+
+// ==,!=
+
+// console.log(10==10);
+// console.log(10=="10");
+
+// console.log(10===10);
+// console.log(10==="10");
+
+// console.log(10!=10);
+// console.log(10!="10");
+
+// console.log(10!==10);
+// console.log(10!=="10");
+
+// && ||
+
+// console.log(10==10 && 10==10);
+// console.log(10==10 && 10!=10);
+
+// console.log(10==10 || 10==10);
+// console.log(10==11 || 10==1);
+
+
+
+
