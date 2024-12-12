e6ee4e81fcd440d79640d681105dd750957bb0f6
jithjayan <jithjayan9341@gmail.com>
2024-12-12 09:04:26 +0530

    update delete

--- idemo.js
+++ idemo.js
@@ -26,8 +26,34 @@ function Display(){
 
         tbody.appendChild(tr)
 
+
+
+        let del_td=document.createElement("td")
+        let del_btn=document.createElement("button")
+        del_btn.innerHTML='delete'
+        del_btn.onclick=function(){
+
+            delete_data(element.id)
+        }
+        del_td.appendChild(del_btn)
+        tr.appendChild(del_td)
+
+        tbody.appendChild(tr)
+
 })
 }
+
+function delete_data(id){
+    data=data.filter((user)=>{
+        if(user.id!=id){
+            return user
+        }
+    })
+    Display()
+}
+
+
+
 
 document.getElementById("add_form").addEventListener('submit',function(event){
 event.preventDefault()
@@ -39,6 +65,7 @@ Display()
 
 })
 
+let edit_var
 function edit_form(id){
     document.getElementById("add_form").style.display='none'
     document.getElementById("edit_form").style.display='block'
@@ -48,7 +75,23 @@ function edit_form(id){
     document.getElementById("e_id").value=edit_data.id
     document.getElementById("e_name").value=edit_data.name
     document.getElementById("e_age").value=edit_data.age
-    
+    edit_var=id
 
 }
+
+document.getElementById("edit_form").addEventListener("submit",function(event){
+    event.preventDefault()
+    let id=document.getElementById("e_id").value
+    let name=document.getElementById("e_name").value
+    let age=document.getElementById("e_age").value
+    data=data.map((user)=>{
+        if(user.id==edit_var){
+            return{...user,id:id,name:name,age:age}
+        }
+        return user
+    })
+    document.getElementById("add_form").style.display='block'
+    document.getElementById("edit_form").style.display='none'
+    Display()
+})
 Display()
